6a1c76014b13f8a00cb0b44a4a6f8d6ad6f7f665
Remssi <zjanne95@gmail.com>
2021-07-22 23:20:35 +0300

    Add first types

---
+++ apps/shared/types.ts
@@ -0,0 +1,30 @@
+export interface Product {
+  name: string;
+  manufacturer: Manufacturer;
+  normalPrice: number;
+  currentPrice?: number;
+  description?: string;
+  // toDo: change to required later, change type if needed
+  img?: File;
+  nutrients?: string[];
+  stock: number;
+  sale?: Sale;
+  categories: Category[];
+}
+
+export interface Manufacturer {
+  name: string;
+}
+
+export interface Sale {
+  multiplier: number;
+  startDate: Date;
+  endDate: Date;
+}
+
+export interface Category {
+  name: string;
+  color: string;
+  textColor: string;
+  description: string;
+}
