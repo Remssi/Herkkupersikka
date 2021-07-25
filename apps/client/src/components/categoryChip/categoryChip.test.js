c6cafbdc83bb64cb74abf5355b8d794171a5afbf
Remssi <zjanne95@gmail.com>
2021-07-25 17:50:25 +0300

    Add first test for category chip component

---
+++ apps/client/src/components/categoryChip/categoryChip.test.js
@@ -0,0 +1,21 @@
+import { render, screen, cleanup } from "@testing-library/react";
+import { CategoryChip } from ".";
+
+afterEach(() => {
+  cleanup();
+});
+
+const testCategoryDetails = {
+  name: "Pizzat",
+  color: "#F5C6AA",
+  textColor: "white",
+  description: "Pizza on paras herkku.",
+};
+
+it("should have the background color as the color given as a prop", () => {
+  render(<CategoryChip categoryDetails={testCategoryDetails} />);
+  const chip = screen.getByText("Pizzat");
+  expect(chip).toHaveStyle("backgroundColor: '#F5C6AA'");
+});
+
+// TODO: write test for clicking the chip to navigate to category search page
