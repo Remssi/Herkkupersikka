7bd8f2fcf52a171b7c6535125eabb6d50114f0e7
Remssi <zjanne95@gmail.com>
2021-07-25 18:20:27 +0300

    Add react-router-dom, add basic routing

--- apps/client/src/App.tsx
+++ apps/client/src/App.tsx
@@ -1,27 +1,36 @@
 import "./App.css";
 import { ProductCard } from "./components";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 const App = () => {
   return (
-    <ProductCard
-      productDetails={{
-        name: "Persikkapizza",
-        normalPrice: 4.99,
-        currentPrice: 3.99,
-        description: "Herkullisin pizza ikinä!",
-        nutrients: ["energia: 376kcal", "suola: 2g"],
-        stock: 25,
-        manufacturer: { name: "Tohtori Persikka" },
-        categories: [
-          {
-            name: "Pizzat",
-            color: "#F5C6AA",
-            textColor: "white",
-            description: "Pizza on paras herkku.",
-          },
-        ],
-      }}
-    />
+    <Router>
+      <Switch>
+        <Route path="/products/:productId"> </Route>
+        <Route path="/categories/:categoryId"></Route>
+        <Route path="/">
+          <ProductCard
+            productDetails={{
+              name: "Persikkapizza",
+              normalPrice: 4.99,
+              currentPrice: 3.99,
+              description: "Herkullisin pizza ikinä!",
+              nutrients: ["energia: 376kcal", "suola: 2g"],
+              stock: 25,
+              manufacturer: { name: "Tohtori Persikka" },
+              categories: [
+                {
+                  name: "Pizzat",
+                  color: "#F5C6AA",
+                  textColor: "white",
+                  description: "Pizza on paras herkku.",
+                },
+              ],
+            }}
+          />
+        </Route>
+      </Switch>
+    </Router>
   );
 };
 
