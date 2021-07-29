50369c3dfec1f1c7efa93250f9f3c2e4b32415f9
Remssi <Zjanne95@gmail.com>
2021-07-29 20:34:51 +0300

    Add base for Menu for Category component

--- apps/client/src/components/header/Categories.tsx
+++ apps/client/src/components/header/Categories.tsx
@@ -1,15 +1,70 @@
-import { Box, Typography } from "@material-ui/core";
+import { useState, useEffect } from "react";
+import {
+  Typography,
+  Button,
+  Menu,
+  MenuItem,
+  ListItemText,
+} from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
+import { Category } from "../../../../shared/types";
 
 interface Props {}
 
-// TODO: make categories as button
 export const Categories: React.FC<Props> = () => {
+  const [categories, setCategories] = useState<Category[]>([]);
+
+  const [anchorElement, setAnchorElement] = useState<null | HTMLElement>(null);
+
+  const handleClick = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorElement(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorElement(null);
+  };
+
   const classes = useClasses();
+
+  useEffect(() => {
+    setCategories([
+      {
+        name: "Pizzat",
+        color: "#F5C6AA",
+        textColor: "white",
+        description: "maukkaita pizzoja",
+      },
+    ]);
+  }, []);
+
+  // TODO: styling of menu and items
   return (
-    <Box className={classes.categories}>
-      <Typography className={classes.categoryText}>Kategoriat</Typography>
-    </Box>
+    <>
+      <Button
+        className={classes.categories}
+        aria-controls="category-menu"
+        aria-haspopup="true"
+        onClick={handleClick}
+      >
+        <Typography className={classes.categoryText}>Kategoriat</Typography>
+      </Button>
+      <Menu
+        elevation={0}
+        getContentAnchorEl={null}
+        anchorOrigin={{ vertical: "bottom", horizontal: "left" }}
+        transformOrigin={{ vertical: "top", horizontal: "left" }}
+        anchorEl={anchorElement}
+        keepMounted
+        open={Boolean(anchorElement)}
+        onClose={handleClose}
+      >
+        {categories.map((category: Category) => (
+          <MenuItem>
+            <ListItemText primary={category.name} />
+          </MenuItem>
+        ))}
+      </Menu>
+    </>
   );
 };
 
